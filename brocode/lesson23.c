72841d19afa23411fdd0ef972b3b2f18ad6777f2
Bbishk128 <bbshk128@gmail.com>
2026-07-21 17:01:33 +0800

    Nested loop

---
+++ brocode/lesson23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Nested loop == Loop доторх loop
+
+int main(){
+
+    // Үржүүлэхийн хүрд
+
+
+    for (int i = 1; i<= 10; i++){
+        for (int j = 1; j <= 10; j++){
+            printf("%3d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
